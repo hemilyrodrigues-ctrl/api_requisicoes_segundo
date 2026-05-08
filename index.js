f18448fc7b3414e1c564c9a8ced708d0dba02435
hemilyrodrigues-ctrl <emilly.candido.souza@escola.pr.gov.br>
2026-05-08 10:56:20 -0300

    new file:   index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express"); 
+const fs =require("fs");
+
+const app = express();
+const port= 3000;
+
+app.use(express.json());
+
+//http://localhost:3000/saudacao?nome=maria
+app.get("/saudacao",(req,res)=>{
+    const nome = req.query.nome;
+    if(!nome){
+        return res.status(404).json(
+        {
+            erro: "nome não foi informado"
+        }
+    )
+  }
+  res.json(
+    {
+        mensagem:`saudações ${nome}!`
+    }
+  )
+})
+
+app.post("/imc",(req,res)=>{
+    const {nome, idade,altura,peso}=req.body
+    if(!nome || !idade ||! altura || !peso){
+        return res.status(404).json({erro: "dados imcompletos"})
+
+    }
+    const imc = peso / (altura*altura)
+    res.json({
+        nome,
+        idade,
+        imc: imc.toFixed(2)
+    })
+})
+
+//finalzao
+app.listen(port,()=> { 
+    console.log(`servidor rodando em http://localhost:${port}`)
+})
