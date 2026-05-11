8cfe85fd2ad2c8809d21eaa5e61864fd7c770b4d
hemilyrodrigues-ctrl <emilly.candido.souza@escola.pr.gov.br>
2026-05-11 09:14:39 -0300

    adicionado enpoint media

--- index.js
+++ index.js
@@ -22,22 +22,47 @@ app.get("/média", (req, res) => {
         }
     )
 })
-
+            
 app.post("/média", (req, res) => {
     const { nome, nota1, nota2 } = req.body
-    if (!nome || !nota1 || !nota2 ) {
+    
+    if (!nome || !nota1 || !nota2 )
+        
+     {
         return res.status(404).json({ erro: "dados imcompletos" })
 
     }
-    const media = 2 / (nota1 + nota2)
+    const media =  (parsefloat(nota1)  + parseFloat(nota2))/2 
+   
     res.json({
         nome,
         nota1,
         nota2,
-        media: media.toFixed(2)
+        mensagem: media >=70? "aprovado":"reprovado",
+        media: parseFloat(media)
+        
+       
     })
+   
 })
+app.post("/imc", (req, res) => {
+    const { nome, idade, altura, peso } = req.body
+    
+    if (!nome || !idade || !altura ||!peso )
+        
+     {
+        return res.status(404).json({ erro: "dados imcompletos" })
 
+    }
+    const imc =  peso /(altura*altura)
+   
+    res.json({
+        nome,
+        idade,
+        imc:toFixed(2)
+    })
+   
+})
 //finalzao
 app.listen(port, () => {
     console.log(`servidor rodando em http://localhost:${port}`)
